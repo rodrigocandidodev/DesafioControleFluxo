51ec4b22d42f68f01707432f5613720edaaad327
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2024-06-05 22:42:39 -0300

    fix: corrigir issue em Scanner - close Scanner

--- src/Contador.java
+++ src/Contador.java
@@ -7,6 +7,7 @@ public class Contador {
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
+        terminal.close();
 		
 		try {
 			contar(parametroUm, parametroDois);
